8a5859a08d144e693d9450a8d3a1b99218a86fc2
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-02-19 14:53:28 +0800

    scrtips/action: add /dev as tmpfs option
    to support APatch .litemode_enable and MKSU .notmpfs

--- module/action.sh
+++ module/action.sh
@@ -54,6 +54,7 @@ set_random_beta() {
 TEMPDIR="$MODDIR/temp" #fallback
 [ -w /sbin ] && TEMPDIR="/sbin/playintegrityfix"
 [ -w /debug_ramdisk ] && TEMPDIR="/debug_ramdisk/playintegrityfix"
+[ -w /dev ] && TEMPDIR="/dev/playintegrityfix"
 mkdir -p "$TEMPDIR"
 cd "$TEMPDIR"
 
